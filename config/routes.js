ad231c38a078ce8ff9faad99cbac7cba83cc342c
yurimaineri <yurimaineri5@gmail.com>
2020-01-02 23:51:14 -0300

    Adicionando rota para sumário

--- config/routes.js
+++ config/routes.js
@@ -8,5 +8,9 @@ module.exports = function(server) {
 
     // rotas API
     const billingCyclesService = require('../api/billingCycle/billingCycleService')
-    
+    // chama a url base router e aplica a rota billing
+    billingCyclesService.register(router, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
