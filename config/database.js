bc8100b3f3940b43f49a9e9a318e93669e05c7e9
yurimaineri <yurimaineri5@gmail.com>
2020-01-02 23:52:02 -0300

    Adicionando erro personalizado para num e enum

--- config/database.js
+++ config/database.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useUnifiedTopology', true)
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+module.exports = mongoose.connect('mongodb://localhost:27017/db_finance')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o mínimo de '{MIN}'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o máximo de '{MAX}'"
+//mongoose.Error.messages.String.enum 
