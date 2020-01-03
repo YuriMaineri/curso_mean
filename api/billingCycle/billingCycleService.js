b06c56405635ee0e982b69836082e656f4e08068
yurimaineri <yurimaineri5@gmail.com>
2020-01-03 00:09:10 -0300

    Criação primeira parte dos erros

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -3,6 +3,19 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if(bundle.errors) {
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
 BillingCycle.route('count', function(req, res, next) {
     BillingCycle.countDocuments(function (error, value) {
         if(error) {
